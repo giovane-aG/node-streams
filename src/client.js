c21ee6cb0945f4d0d8e051981c2c0667a9052c20
Giovane <giovaneaalmeida27@gmail.com>
2024-05-22 12:06:16 -0300

    feat: add to upper case pipe

--- src/client.js
+++ src/client.js
@@ -15,22 +15,33 @@ const consume = async () => {
 
 const stream = await consume()
 
-stream.pipe(
-  new Transform({
-    transform(chunk, encoding, callback) {
-      const parsedData = JSON.parse(chunk)
-      const myNumber = /\d+/.exec(parsedData.name)[0]
+stream
+  .pipe(
+    new Transform({
+      transform(chunk, encoding, callback) {
+        const parsedData = JSON.parse(chunk)
+        const myNumber = /\d+/.exec(parsedData.name)[0]
 
-      let name = parsedData.name
-      if (myNumber % 2 === 0) {
-        name = name.concat(' is even')
-      } else name = name.concat(' is odd')
+        let name = parsedData.name
+        if (myNumber % 2 === 0) {
+          name = name.concat(' is even')
+        } else name = name.concat(' is odd')
 
-      parsedData.name = name
-      callback(null, JSON.stringify(parsedData))
-    }
-  })
-)
+        parsedData.name = name
+        callback(null, JSON.stringify(parsedData))
+      }
+    })
+  )
+  .pipe(
+    new Transform({
+      transform(chunk, encoding, callback) {
+        const parsedData = JSON.parse(chunk.toString())
+        parsedData.name = parsedData.name.toUpperCase()
+
+        callback(null, JSON.stringify(parsedData))
+      }
+    })
+  )
   .pipe(
     new Writable({
       write(chunk, encoding, callback) {
